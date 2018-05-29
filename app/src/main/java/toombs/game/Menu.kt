381834f21ba8902d736fd072c06737d76bcced75
RobToombs <toombs.rob@gmail.com>
2018-05-28 22:25:59 -0400

    Updated main activity, added game activity

--- app/src/main/java/toombs/game/Menu.kt
+++ app/src/main/java/toombs/game/Menu.kt
@@ -3,10 +3,15 @@ package toombs.game
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Menu : AppCompatActivity() {
+class Menu : AppCompatActivity()
+{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun continuous() {
+        
     }
 }
