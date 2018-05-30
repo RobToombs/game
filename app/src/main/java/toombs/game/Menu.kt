3c9aa21c7a5d0aab13cc1bebc2153a5cbe2d944b
RobToombs <toombs.rob@gmail.com>
2018-05-29 22:41:39 -0400

    added button handling to continuous to bring up new game activity

--- app/src/main/java/toombs/game/Menu.kt
+++ app/src/main/java/toombs/game/Menu.kt
@@ -1,7 +1,9 @@
 package toombs.game
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Menu : AppCompatActivity()
 {
@@ -9,9 +11,11 @@ class Menu : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-    }
 
-    fun continuous() {
-        
+        val continuous = findViewById<Button>(R.id.continuous)
+        continuous.setOnClickListener {
+            val intent = Intent(this, Game::class.java)
+            startActivity(intent)
+        }
     }
 }
