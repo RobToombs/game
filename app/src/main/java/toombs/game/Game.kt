80c1d8379e995a2ee3fed7bf11b04f6525ebd0e1
RobToombs <toombs.rob@gmail.com>
2018-06-17 22:36:30 -0400

    removed bar from game view, started work on 'hero' class

--- app/src/main/java/toombs/game/Game.kt
+++ app/src/main/java/toombs/game/Game.kt
@@ -2,11 +2,13 @@ package toombs.game
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Window
 
 class Game : Activity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_game)
     }
 }
---
+++ app/src/main/java/toombs/game/Hero.kt
@@ -0,0 +1,9 @@
+package toombs.game
+
+import android.content.Context
+import android.view.View
+
+class Hero : View
+{
+    constructor(ctx: Context) : super(ctx)
+}
